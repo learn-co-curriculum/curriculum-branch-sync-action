ff7a4262c2a59b72a03b883034b5b5b3e39bfadb
Alex Griffith <alexalexgriffith@gmail.com>
2019-10-04 16:02:51 -0400

    Add workflow

---
+++ curriculum_resources/solution.py
@@ -0,0 +1,81 @@
+import json
+
+# CONSTANTS
+SOLUTION_TAG = "__SOLUTION__"
+CURRICULUM_BRANCH = "curriculum"
+MASTER_BRANCH = "master"
+SOLUTION_BRANCH = "solution"
+
+# FUNCTIONS
+def get_notebook_json(filename="index.ipynb"):
+    with open(filename, 'r') as f:
+        data = json.load(f)
+    return data
+
+
+def is_markdown_cell(cell):
+    return cell["cell_type"] == "markdown"
+
+
+def contains_tag(line):
+    # returns true for '# __SOLUTION__' or '#__SOLUTION__'
+    return any(tag in line.strip().split(" ") for tag in [SOLUTION_TAG, f"#{SOLUTION_TAG}"])
+
+
+def is_solution_cell(cell):
+    if cell["cell_type"] != "code":
+        return False
+
+    # does any line of the cell have the SOLUTION tag anywhere in it
+    found_tag = [True for line in cell["source"] if contains_tag(line)]
+
+    return bool(len(found_tag))
+
+
+# removes __SOLUTON__ line from tagged code cells
+def untag(cell):
+    if cell["cell_type"] != "code":
+        return cell
+
+    source = [line for line in cell["source"] if not contains_tag(line)]
+
+    cell.update({"source": source})
+    return cell
+
+
+def create_master_notebook(nb):
+    cells = [
+        cell for cell in nb["cells"] if for_master(cell)
+    ]
+
+    nb.update({"cells": cells})
+    return nb
+
+
+def for_master(cell):
+    return is_markdown_cell(cell) or not is_solution_cell(cell)
+
+
+def for_sol(cell):
+    return is_markdown_cell(cell) or is_solution_cell(cell)
+
+
+def create_sol_notebook(nb):
+    cells = [
+        untag(cell) for cell in nb["cells"] if for_sol(cell)
+    ]
+
+    nb.update({"cells": cells})
+    return nb
+
+def write_new_notebook(branch, notebook):
+    f = open(f"{branch}_index.ipynb", "w")
+    f.write(json.dumps(notebook))
+    f.close()
+
+# RUN
+# ======================
+notebook_json   = get_notebook_json()
+sol_notebook = create_sol_notebook(dict(notebook_json)) # pass a copy
+
+write_new_notebook(SOLUTION_BRANCH, sol_notebook)
